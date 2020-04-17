027c73f1f3a0148068d053aea7d34941a9a81d6b
Ivan.Malinin <yjn.malinin@gmail.com>
2020-04-17 08:26:40 +0400

    обновил сборку

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,18 @@
 <template>
   <div id="app">
-    <img width="25%" src="./assets/logo.png">
-    <HelloWorld msg="Hello Vue in CodeSandbox!" />
+    <graph-wrapper/>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld";
-
+import GraphWrapper from "./components/GraphWrapper";
 export default {
-  name: "App",
+  name: "app",
   components: {
-    HelloWorld
+    GraphWrapper
   }
 };
 </script>
 
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
